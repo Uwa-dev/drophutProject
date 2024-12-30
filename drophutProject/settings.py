642f984883ee42986428a4877847f19e544b7c0f
Joy-dev <omoikejoy34@gmail.com>
2024-12-30 13:17:59 +0100

    Update

--- drophutProject/settings.py
+++ drophutProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#h(gv(hy*v4#towi)mzj)dm25)*#ej*3j7vc=c1b9o@%xp*nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
